7c604c267fcba6c4932efd0e3408b79ca726dbdf
mohmoniem281 <mohmoniem281@gmail.com>
2021-02-22 13:09:15 -0500

    we
    we

--- Setup.py
+++ Setup.py
@@ -16,7 +16,7 @@ setup(
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'http://businessez.net/cornershop.gz',    # I explain this later on
+  download_url = 'https://github.com/mohmoniem281/modytest/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
