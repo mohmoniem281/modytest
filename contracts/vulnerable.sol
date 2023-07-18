5bdc9290057a2cc5bf8b522b4c6699a9902a32aa
Mohamed Mohamed <mohamedmohamed@Mohameds-MacBook-Pro.local>
2023-07-18 16:36:50 +0300

    d

--- contracts/vulnerable.sol
+++ contracts/vulnerable.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.10;
 
+contract Vuln{
+    uint public balance;
+    constructor()public{
+        balance = 10 ether;
+    }
+    function reentrancy(address payable receiver,uint amount)external{
+        receiver.transfer(amount);
+        balance-=amount;
+
+    }
+}
